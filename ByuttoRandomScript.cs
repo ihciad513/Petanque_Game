296899c5d97584074501126de80ddb8344c5024d
ihciad513 <75005201+ihciad513@users.noreply.github.com>
2020-12-30 23:45:04 +0900

    Add files via upload

---
+++ ByuttoRandomScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ByuttoRandomScript : MonoBehaviour
+{
+    public GameObject byutto;
+    bool once = true;
+    public int byuttocount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (once == true)
+        {
+            byuttocount += 1;
+            if (byuttocount == 1)
+            {
+                once = false;
+                Instabyutto();
+            }
+            
+        }
+        
+    }
+    void Instabyutto()
+    {
+        float x = Random.Range(-4.0f, 3.5f);
+        float z = Random.Range(-2.0f, 10.0f);
+        Instantiate(byutto, new Vector3(x, 1.9f, z), Quaternion.identity);
+    }
+}
---
+++ ScoreManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ScoreManager : MonoBehaviour
+{
+    public Text scoretext;
+    int score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = PlayerPrefs.GetInt("PS");
+        scoretext.text = score.ToString();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
